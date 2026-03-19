b6e6c8f19836f8c2981cf8161e60c7660d80a16a
gavimg <gavigadagi@gmail.com>
2026-03-19 08:53:13 +0530

    chore: sync package metadata and build config

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,61 @@
 
 module.exports = {
   entry: './src/index.ts',
-  resolve: { extensions: ['.ts', '.tsx', '.js'] },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
   module: {
     rules: [
-      { test: /\.tsx?$/, use: [{ loader: 'ts-loader', options: { transpileOnly: true } }], exclude: /node_modules/ },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: { transpileOnly: true },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader'], // ✅ removed style-loader
+      },
     ],
   },
   externals: {
-    react:                { commonjs: 'react', commonjs2: 'react', amd: 'React', root: 'React' },
-    'react-dom':          { commonjs: 'react-dom', commonjs2: 'react-dom' },
-    'react-router-dom':   { commonjs: 'react-router-dom', commonjs2: 'react-router-dom' },
-    '@gadagi/types':         { commonjs: '@gadagi/types', commonjs2: '@gadagi/types' },
-    '@gadagi/design-system': { commonjs: '@gadagi/design-system', commonjs2: '@gadagi/design-system' },
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+    'react-router-dom': {
+      commonjs: 'react-router-dom',
+      commonjs2: 'react-router-dom',
+      amd: 'react-router-dom',
+    },
+    '@gadagi/types': {
+      commonjs: '@gadagi/types',
+      commonjs2: '@gadagi/types',
+    },
+    '@gadagi/design-system': {
+      commonjs: '@gadagi/design-system',
+      commonjs2: '@gadagi/design-system',
+    },
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: { name: 'gadagiUiNavigation', type: 'umd' },
+    library: {
+      name: 'gadagiUiNavigation',
+      type: 'umd',
+    },
     globalObject: 'this',
     clean: true,
   },
